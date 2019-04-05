d680ba1558a4d506336357dee4ed6fa63f89db6d
Tim Veil <tjveil@gmail.com>
2019-04-05 09:23:52 -0400

    updating comments

--- docker/run.sh
+++ docker/run.sh
@@ -3,7 +3,7 @@
 ## build all images
 docker-compose build --no-cache
 
-## start source database and load balancer
+## start database and load balancer
 docker-compose up --no-start
 docker-compose start roach-0
 docker-compose start roach-1
@@ -13,6 +13,6 @@ docker-compose start lb
 ## wait for cockroach to initialize
 sleep 5
 
-## create backup database
+## create database
 docker-compose exec roach-0 /cockroach/cockroach sql --insecure --execute="CREATE DATABASE examples;"
 docker-compose exec roach-0 /cockroach/cockroach sql --insecure --execute="SET CLUSTER SETTING server.remote_debugging.mode = \"any\";"
