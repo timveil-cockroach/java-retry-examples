bf9732c962da9130d9b6a8ad8f8298b1e62ad276
Tim Veil <3260845+timveil@users.noreply.github.com>
2021-01-18 09:24:57 -0500

    updating harness to remove warnings; removing static lb image

--- basic/src/main/java/io/crdb/examples/retry/basic/BasicExampleHarness.java
+++ basic/src/main/java/io/crdb/examples/retry/basic/BasicExampleHarness.java
@@ -2,17 +2,16 @@ package io.crdb.examples.retry.basic;
 
 import org.postgresql.ds.PGSimpleDataSource;
 
-import java.sql.SQLException;
-
 class BasicExampleHarness {
 
     public static void main(String[] args) {
 
         // Create DataSource
         final PGSimpleDataSource ds = new PGSimpleDataSource();
-        ds.setServerName("localhost");
-        ds.setPortNumber(26257);
+        ds.setServerNames(new String[]{"localhost"});
+        ds.setPortNumbers(new int[]{26257});
         ds.setDatabaseName("java_retry_examples");
+        ds.setApplicationName("java_retry_examples");
         ds.setUser("root");
         ds.setPassword(null);
 
